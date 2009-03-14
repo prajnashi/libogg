ab800b620c724e9ff5694c950d7d766773df4a53
Edward Hervey <bilboed@bilboed.com>
2009-03-14 15:44:58 +0100

    wrap libogg

---
+++ src/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= 			\
+	framing.c	\
+	bitwise.c
+
+# LOCAL_SHARED_LIBRARIES := 	\
+#     libglib-2.0             	\
+#     libgthread-2.0          	\
+#     libgmodule-2.0          	\
+#     libgobject-2.0
+
+LOCAL_MODULE:= libogg
+
+LOCAL_C_INCLUDES := 				\
+	$(LOCAL_PATH)	\
+	$(LIBOGG_TOP)/include
+
+include $(BUILD_SHARED_LIBRARY)
